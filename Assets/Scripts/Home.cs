aba79e730c1b06d090c9af36f9e65d0f22fdb39b
xianqiang.guo <xianqiang.guo@ihanysoft.com>
2019-08-09 12:37:46 +0800

    添加Splash,Home页,启动页;

---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class Home : MonoBehaviour
+{
+    private int count = 0;
+    private static string key_count = "key_count";
+
+    private void Start()
+    {
+        count = GameDataManager.GetInt(key_count);
+    }
+
+    public void GoMainGame()
+    {
+        //播放音效
+        AudioSource source = GetComponent<AudioSource>();
+        AudioClip newClip = Resources.Load<AudioClip>("Music/enter_game");
+        source.clip = newClip;
+        source.Play();
+        count += 1;
+        GameDataManager.SetInt(key_count, count);
+        print(count);
+        //SceneManager.LoadScene(1);
+    }
+}
